3aabf64a445f692a1b0b24791a9ab26a4bbef7b7
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-11-29 09:36:58 +0500

    os.js module

---
+++ modules/os.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// bu qaysi sitemada kirayotganimizni aytib beradi
+// console.log(os.platform());
+
+// bu sistema haqida ma'lumotlar chiqarib beradi cpularimizni
+// console.log(os.cpus());
+
+// bu notebukimizni arxitekturasi
+// console.log(os.arch());
+
+//free memmory  xotirada qancha joy borligni ko'rsatadi
+// console.log(os.freemem());
+
+//tottal memory bu ummumiy joyni ko'rsatib berad
+// console.log(os.totalmem());
+
+//entry point bu users/Ascer degan joyini chiqarib beradi kompyuter kirish joyini
+console.log(os.homedir());
